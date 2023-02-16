4627ab52966b920e6e27fa67ddd16a1cac83bf28
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-02-16 17:18:14 -0300

    init project

---
+++ src/main/java/com/rafaelmgr12/medvollapi/MedvollapiApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelmgr12.medvollapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedvollapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedvollapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelmgr12/medvollapi/MedvollapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelmgr12.medvollapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedvollapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
