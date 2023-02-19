b7322b42ebc48a70aa6b16bf02c43b8f65a4d55f
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2023-02-19 12:16:59 -0300

    [chore] add query method

--- src/main/java/com/rafaelmgr12/medvollapi/repository/DoctorRepository.java
+++ src/main/java/com/rafaelmgr12/medvollapi/repository/DoctorRepository.java
@@ -1,8 +1,12 @@
 package com.rafaelmgr12.medvollapi.repository;
 
 import com.rafaelmgr12.medvollapi.entity.Doctor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
 
+    Page<Doctor> findAllByActiveTrue(Pageable pageable);
 }
