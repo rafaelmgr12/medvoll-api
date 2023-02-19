0f1af3f1ef8cac1ebe3f171e4a92964ab66e245a
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2023-02-19 10:51:28 -0300

    [feat] add get list doctors using pageable

--- src/main/java/com/rafaelmgr12/medvollapi/controller/DoctorController.java
+++ src/main/java/com/rafaelmgr12/medvollapi/controller/DoctorController.java
@@ -1,13 +1,15 @@
 package com.rafaelmgr12.medvollapi.controller;
 
+import com.rafaelmgr12.medvollapi.dto.ListDataDoctorDTO;
 import com.rafaelmgr12.medvollapi.dto.RegisterDoctorDTO;
 import com.rafaelmgr12.medvollapi.entity.Doctor;
 import com.rafaelmgr12.medvollapi.repository.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -19,6 +21,12 @@ public class DoctorController {
     @PostMapping
     public void register(@RequestBody RegisterDoctorDTO dto) {
         repository.save(new Doctor(dto));
+    }
+
+    @GetMapping
+    public Page<ListDataDoctorDTO> list(Pageable pageable) {
+        return repository.findAll(pageable).map(ListDataDoctorDTO::new);
+
     }
 
 }
