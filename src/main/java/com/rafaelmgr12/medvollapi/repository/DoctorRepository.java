ee7795934155f6014c9a3d2b62b8c57aabc0e59c
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2023-02-20 19:41:19 -0300

    [feat] added method to detail only actives users

--- src/main/java/com/rafaelmgr12/medvollapi/repository/DoctorRepository.java
+++ src/main/java/com/rafaelmgr12/medvollapi/repository/DoctorRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
 
     Page<Doctor> findAllByActiveTrue(Pageable pageable);
+
+    Optional<Doctor> findByIdAndActiveIsTrue(Long aLong);
 }
