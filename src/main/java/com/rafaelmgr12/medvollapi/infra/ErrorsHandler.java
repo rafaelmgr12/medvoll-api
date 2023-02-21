320cdf4e44d35487c817111da04dd3ed23037ae7
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2023-02-21 11:59:16 -0300

    [feat] add error handler

---
+++ src/main/java/com/rafaelmgr12/medvollapi/infra/ErrorsHandler.java
@@ -0,0 +1,15 @@
+package com.rafaelmgr12.medvollapi.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorsHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handlerError404(){
+        return ResponseEntity.notFound().build();
+    }
+}
