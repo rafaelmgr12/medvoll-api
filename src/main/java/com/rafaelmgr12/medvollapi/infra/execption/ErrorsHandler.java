a576d85d4f26c9274ac06ac4d4d31c5ab84b8bd1
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-02-24 12:47:47 -0300

    [refactor] create a new package

--- src/main/java/com/rafaelmgr12/medvollapi/infra/execption/ErrorsHandler.java
+++ src/main/java/com/rafaelmgr12/medvollapi/infra/execption/ErrorsHandler.java
@@ -1,4 +1,4 @@
-package com.rafaelmgr12.medvollapi.infra;
+package com.rafaelmgr12.medvollapi.infra.execption;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
