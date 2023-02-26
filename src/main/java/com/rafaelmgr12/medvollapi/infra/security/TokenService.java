c6572086f88ae03afc3be2fc4bef5c360d3d6156
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2023-02-26 18:39:16 -0300

    [feat] added method to extract the jwt token from header

--- src/main/java/com/rafaelmgr12/medvollapi/infra/security/TokenService.java
+++ src/main/java/com/rafaelmgr12/medvollapi/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.rafaelmgr12.medvollapi.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.rafaelmgr12.medvollapi.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,19 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception){
         throw new RuntimeException("erro ao gerar token jwt", exception);}
+    }
+
+    public String getSubject(String tokenJWT){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("erro ao gerar token jwt", exception);
+        }
     }
 
     private Instant expireDate() {
