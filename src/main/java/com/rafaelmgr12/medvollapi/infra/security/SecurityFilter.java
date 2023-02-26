1a004787dd515b51af3ccb2288a1cf5bb83cc6dd
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2023-02-26 20:34:14 -0300

    [feat] added filter configuration

---
+++ src/main/java/com/rafaelmgr12/medvollapi/infra/security/SecurityFilter.java
@@ -0,0 +1,48 @@
+package com.rafaelmgr12.medvollapi.infra.security;
+
+import com.rafaelmgr12.medvollapi.domain.user.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenJWT = retrieveToken(request);
+        if (tokenJWT != null) {
+            String subject = tokenService.getSubject(tokenJWT);
+            UserDetails user = repository.findByLogin(subject);
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String retrieveToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if (token != null) {
+            return token.replace("Bearer ", "");
+        }
+
+        return null;
+    }
+}
