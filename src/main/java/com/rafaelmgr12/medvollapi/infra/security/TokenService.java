854e064bd7ebe6f32e358014091a49961cd447b2
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2023-02-26 15:26:47 -0300

    [feat] add token service

---
+++ src/main/java/com/rafaelmgr12/medvollapi/infra/security/TokenService.java
@@ -0,0 +1,35 @@
+package com.rafaelmgr12.medvollapi.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.rafaelmgr12.medvollapi.domain.user.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(user.getLogin())
+                    .withExpiresAt(expireDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+        throw new RuntimeException("erro ao gerar token jwt", exception);}
+    }
+
+    private Instant expireDate() {
+        return LocalDateTime.now().plusMinutes(30).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
