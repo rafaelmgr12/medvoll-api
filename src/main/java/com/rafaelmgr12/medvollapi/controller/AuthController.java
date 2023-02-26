c50ec5fe314a2459686bfcea4dbf1f30cd188817
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2023-02-26 15:27:04 -0300

    [refactor] add token service in the class

--- src/main/java/com/rafaelmgr12/medvollapi/controller/AuthController.java
+++ src/main/java/com/rafaelmgr12/medvollapi/controller/AuthController.java
@@ -1,11 +1,15 @@
 package com.rafaelmgr12.medvollapi.controller;
 
 import com.rafaelmgr12.medvollapi.domain.user.AuthDataDTO;
+import com.rafaelmgr12.medvollapi.domain.user.User;
+import com.rafaelmgr12.medvollapi.infra.security.JWTTokenDTO;
+import com.rafaelmgr12.medvollapi.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +21,17 @@ public class AuthController {
 
     @Autowired
     private AuthenticationManager manager;
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity<Object> login(@RequestBody @Valid AuthDataDTO data){
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(data.login(), data.password());
-        manager.authenticate(token);
-        return ResponseEntity.ok().build();
+        Authentication auth = manager.authenticate(token);
+
+        String jwt = tokenService.generateToken((User) auth.getPrincipal());
+
+        return ResponseEntity.ok( new JWTTokenDTO(jwt));
     }
 
 }
