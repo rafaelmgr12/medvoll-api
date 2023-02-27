7a7e1384ac52aea43ed396105fa9208e900b1c4b
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-02-27 17:38:16 -0300

    [fix] syntax error in the query

--- src/main/resources/db/migration/V6__create-tables-appointment.sql
+++ src/main/resources/db/migration/V6__create-tables-appointment.sql
@@ -1,11 +1,12 @@
-create table appointments{
+create table appointments
+(
 
-    id bigint not null auto_increment,
-    doctor_id bigint not null,
-    patient_id bigint not null,
-    date datetime not null,
+    id         bigint   not null auto_increment,
+    doctor_id  bigint   not null,
+    patient_id bigint   not null,
+    date       datetime not null,
 
     primary key (id),
-    constraint fk_appointments_doctors_id foreign key (doctor_id) references doctors(id),
-    constraint fk_appointments_patients_id foreign key (patient_id) references patients(id)
-    }
+    constraint fk_appointments_doctors_id foreign key (doctor_id) references doctors (id),
+    constraint fk_appointments_patients_id foreign key (patient_id) references patients (id)
+)
