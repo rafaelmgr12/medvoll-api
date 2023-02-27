818d190d911b237d834fc9a6923ee55a6c442433
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-02-27 17:47:57 -0300

    [feat] created Appointment Controller

---
+++ src/main/java/com/rafaelmgr12/medvollapi/controller/AppointmentController.java
@@ -0,0 +1,27 @@
+package com.rafaelmgr12.medvollapi.controller;
+
+import com.rafaelmgr12.medvollapi.domain.appointment.AppointmentDetailDTO;
+import com.rafaelmgr12.medvollapi.domain.appointment.DataSchedulingConsultationDTO;
+import com.rafaelmgr12.medvollapi.domain.appointment.ScheduleAppointment;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consultas")
+public class AppointmentController {
+
+    @Autowired
+    private ScheduleAppointment scheduleAppointment;
+    @PostMapping
+    @Transactional
+    public ResponseEntity<AppointmentDetailDTO> schedule(@RequestBody @Valid DataSchedulingConsultationDTO data){
+        scheduleAppointment.schedule(data);
+        return ResponseEntity.ok(new AppointmentDetailDTO(null, null, null, null));
+    }
+}
