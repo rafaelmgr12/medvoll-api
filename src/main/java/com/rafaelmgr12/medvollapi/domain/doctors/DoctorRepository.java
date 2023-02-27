5b1f407a9980cbff9920f401382e297a05412645
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-02-27 17:44:56 -0300

    [feat] create method to query a random doctor

--- src/main/java/com/rafaelmgr12/medvollapi/domain/doctors/DoctorRepository.java
+++ src/main/java/com/rafaelmgr12/medvollapi/domain/doctors/DoctorRepository.java
@@ -3,7 +3,9 @@ package com.rafaelmgr12.medvollapi.domain.doctors;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 
@@ -12,4 +14,21 @@ public interface DoctorRepository extends JpaRepository<Doctor, Long> {
     Page<Doctor> findAllByActiveTrue(Pageable pageable);
 
     Optional<Doctor> findByIdAndActiveIsTrue(Long aLong);
+
+    @Query("""
+        select d from Doctor d
+        where
+        d.active = true
+        and
+        d.speciality = :speciality
+        and
+        d.id not in(
+        select a.doctor.id from Appointment a
+        where
+        a.date = :date
+        )
+        order by rand()
+        limit 1
+        """)
+    Doctor chooseRandomFreeDoctorOnFreeDate(Speciality speciality, LocalDateTime date);
 }
