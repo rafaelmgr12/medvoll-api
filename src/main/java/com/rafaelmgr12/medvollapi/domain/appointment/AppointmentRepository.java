a704151f297f0e56f355578137f726c265984937
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-02-28 16:45:44 -0300

    [feat] add new queries

--- src/main/java/com/rafaelmgr12/medvollapi/domain/appointment/AppointmentRepository.java
+++ src/main/java/com/rafaelmgr12/medvollapi/domain/appointment/AppointmentRepository.java
@@ -2,5 +2,10 @@ package com.rafaelmgr12.medvollapi.domain.appointment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    Boolean existsByDoctorIdAndDate(Long doctorId, LocalDateTime date);
+
+    Boolean existsByPatientIdAndDateBetween(Long patientId, LocalDateTime firstHour, LocalDateTime lastHour);
 }
