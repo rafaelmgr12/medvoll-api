cfa36868918230d0fc1b0ec26d01f6e8aeb09d89
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-02-28 16:45:41 -0300

    [feat] add new queries

--- src/main/java/com/rafaelmgr12/medvollapi/domain/patient/PatientRepository.java
+++ src/main/java/com/rafaelmgr12/medvollapi/domain/patient/PatientRepository.java
@@ -3,9 +3,14 @@ package com.rafaelmgr12.medvollapi.domain.patient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
     Page<Patient> findAllByActiveTrue(Pageable pageable);
 
+    @Query("""
+        select p.active from Patient p where p.id = :patientId
+""")
+    Boolean findActiveById(Long patientId);
 }
