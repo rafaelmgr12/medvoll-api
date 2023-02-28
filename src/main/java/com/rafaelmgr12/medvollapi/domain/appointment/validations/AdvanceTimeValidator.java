8ff95b02e8736fc21cff9351bd55db8228f26cdf
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-02-28 14:12:47 -0300

    [feat] implement the validation of the cancel consulting

---
+++ src/main/java/com/rafaelmgr12/medvollapi/domain/appointment/validations/AdvanceTimeValidator.java
@@ -0,0 +1,22 @@
+package com.rafaelmgr12.medvollapi.domain.appointment.validations;
+
+import com.rafaelmgr12.medvollapi.domain.appointment.DataSchedulingConsultationDTO;
+import com.rafaelmgr12.medvollapi.infra.execption.ValidationException;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class AdvanceTimeValidator implements AppointmentSchedulingValidator{
+
+    @Override
+    public void validate(DataSchedulingConsultationDTO data) {
+        LocalDateTime appointmentDate = data.date();
+        LocalDateTime now = LocalDateTime.now();
+        Long differenceBetweenDates = Duration.between(now, appointmentDate).toMinutes();
+
+        if (differenceBetweenDates < 30) {
+            throw new ValidationException("Consulta deve ser agendada com antecedência mínima de 30 minutos");
+        }
+
+    }
+}
