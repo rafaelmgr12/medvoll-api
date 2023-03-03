22bbfe25d9aaec91bd6d370c4cf5f8194bb939aa
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-03-03 15:21:34 -0300

    [feat] add new handlers validation

--- src/main/java/com/rafaelmgr12/medvollapi/infra/execption/ErrorsHandler.java
+++ src/main/java/com/rafaelmgr12/medvollapi/infra/execption/ErrorsHandler.java
@@ -2,6 +2,8 @@ package com.rafaelmgr12.medvollapi.infra.execption;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,5 +13,23 @@ public class ErrorsHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Object> handlerError404(){
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handlerError400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(ErrorDataValidation::new).toList());
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<Object> handlerErrorBusinessRule(ValidationException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    private record ErrorDataValidation(String field, String message) {
+     public ErrorDataValidation(FieldError erro){
+         this(erro.getField(), erro.getDefaultMessage());
+     }
+
     }
 }
