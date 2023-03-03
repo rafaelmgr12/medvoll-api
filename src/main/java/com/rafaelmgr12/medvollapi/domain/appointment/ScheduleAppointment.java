4d29bb66615201b75a70b6cfb9a3030d0bd3493d
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-03-03 15:23:25 -0300

    [feat] add validations methods

--- src/main/java/com/rafaelmgr12/medvollapi/domain/appointment/ScheduleAppointment.java
+++ src/main/java/com/rafaelmgr12/medvollapi/domain/appointment/ScheduleAppointment.java
@@ -1,5 +1,6 @@
 package com.rafaelmgr12.medvollapi.domain.appointment;
 
+import com.rafaelmgr12.medvollapi.domain.appointment.validations.AppointmentSchedulingValidator;
 import com.rafaelmgr12.medvollapi.domain.doctors.Doctor;
 import com.rafaelmgr12.medvollapi.domain.doctors.DoctorRepository;
 import com.rafaelmgr12.medvollapi.domain.patient.Patient;
@@ -7,6 +8,8 @@ import com.rafaelmgr12.medvollapi.domain.patient.PatientRepository;
 import com.rafaelmgr12.medvollapi.infra.execption.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
@@ -21,16 +24,27 @@ public class ScheduleAppointment {
     @Autowired
     private PatientRepository patientRepository;
 
-    public void schedule(DataSchedulingConsultationDTO data){
+    @Autowired
+    private List<AppointmentSchedulingValidator> validators;
+
+    public AppointmentDetailDTO schedule(DataSchedulingConsultationDTO data){
         if (!patientRepository.existsById(data.patientId())){
             throw new ValidationException("Paciente não encontrado");}
         if (data.doctorId() != null && !doctorRepository.existsById(data.doctorId())){
             throw new ValidationException("Médico não encontrado");}
 
+        validators.forEach(validator -> validator.validate(data));
+
         Patient patient = patientRepository.getReferenceById(data.patientId());
         Doctor doctor = chooseDoctor(data);
+        if (doctor == null){
+            throw new ValidationException("Não há médicos disponíveis para a especialidade informada nesta data");
+        }
+
         Appointment appointment = new Appointment(null, doctor, patient, data.date());
         appointmentRepository.save(appointment);
+
+        return new AppointmentDetailDTO(appointment);
     }
 
     private Doctor chooseDoctor (DataSchedulingConsultationDTO data){
