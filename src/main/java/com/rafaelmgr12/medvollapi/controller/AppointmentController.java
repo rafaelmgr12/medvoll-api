a2d57fc3a36280b0ed44ca1e8f9e3572084d9e37
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-03-04 09:25:46 -0300

    [refactor] remove blanklines

--- src/main/java/com/rafaelmgr12/medvollapi/controller/AppointmentController.java
+++ src/main/java/com/rafaelmgr12/medvollapi/controller/AppointmentController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/consultas")
 @SecurityRequirement(name = "bearer-key")
-
 public class AppointmentController {
 
     @Autowired
